8c8489dbaa0a5f8f1eddaec97ccd42e5dbc4139f
Adam Stacoviak <adam@gethandcrafted.com>
2009-09-02 01:43:01 -0500

    [Foundation] Created a Sass grid using 1kb 960, updated the application views markup to follow the #page_header, #page_body and #page_sidebar pattern, cleaned up site.sass refactoring and adding in the grid mixins, nice clean new style, moved notices to the notices and errors mixin, updated and added default notice partials

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,6 +1,8 @@
 class UserSessionsController < ApplicationController
   before_filter :require_no_user, :only => [:new, :create]
   before_filter :require_user, :only => :destroy
+  
+  layout "application"
 
   def new
     @user_session = UserSession.new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,6 @@ class UsersController < ApplicationController
   before_filter :require_no_user, :only => [:new, :create]
   before_filter :require_user, :only => [:show, :edit, :update]
   
-  layout "user_sessions"
-  
   def new
     @user = User.new
   end
