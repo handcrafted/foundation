7d80547b015663de4bce63495fbcae75e1b33237
Adam Stacoviak <adam@gethandcrafted.com>
2009-09-16 10:22:01 -0500

    [Foundation] Updated Compass settings to output as expanded and to supress line comments in the source

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,9 @@
 # every request.  This slows down response time but is perfect for development
 # since you don't have to restart the webserver when you make code changes.
 config.cache_classes = false
+config.after_initialize do
+  Sass::Plugin.options[:line_comments] = false
+end 
 
 # Log error messages when you accidentally call methods on nil.
 config.whiny_nils = true
