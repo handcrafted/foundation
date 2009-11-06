eca6f1e3e4fdf9f07818278c8da2b034076f1ba3
Josh Owens <joshua.owens@gmail.com>
2009-11-06 16:21:17 -0500

    [Foundation] Removing old gems.

--- vendor/gems/chriseppstein-compass-0.8.5/lib/compass/sass_extensions/functions/enumerate.rb
+++
@@ -1,6 +0,0 @@
-module Compass::SassExtensions::Functions::Enumerate
-  def enumerate(prefix, from, through)
-    selectors = (from.value..through.value).map{|i| "#{prefix.value}-#{i}"}.join(", ")
-    Sass::Script::String.new(selectors)
-  end
-end
--- vendor/gems/javan-whenever-0.2.2/lib/version.rb
+++
@@ -1,9 +0,0 @@
-module Whenever
-  module VERSION #:nodoc:
-    MAJOR = 0
-    MINOR = 2
-    TINY  = 2
-
-    STRING = [MAJOR, MINOR, TINY].join('.')
-  end
-end unless defined?(Whenever::VERSION)
